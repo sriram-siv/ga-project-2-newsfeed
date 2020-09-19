b9740f3fb7941a8a343045ad4e416100d083e5bf
SimonN <65023744+Simon994@users.noreply.github.com>
2020-09-19 17:55:47 +0100

    loading message and slide on top stories load

--- src/components/TopStories.js
+++ src/components/TopStories.js
@@ -7,46 +7,53 @@ import { getTopStories } from '../lib/api'
 class TopStories extends React.Component{
   state= {
     stories: null,
-    selected: 'general'
+    selected: 'general',
+    loading: true
   }
 
   async componentDidMount() {
     const response = await getTopStories('general')
     this.setState({
-      stories: response.data
+      stories: response.data,
+      loading: false
     })
   }
 
-  handleClick = async event =>{
+  handleClick = event =>{
+    // this.setState({
+    //   loading: true
+    // })
+
+    this.handleLoad(event)
+  }
+
+  handleLoad = async event =>{
     const response = await getTopStories(event.target.textContent)
     
     this.setState({
-      stories: response.data
+      stories: response.data,
+      loading: false
     })
-    
   }
 
   render(){
     
     return (
       <>
-        <div className="header"></div>
-        <div className="navbar topstory-nav form-container" role="navigation" aria-label="sub navigation">
-          <div className="navbar-brand">
-            <SubNavBtns onClick={this.handleClick} className="button is-large"/>
-            {/* <button onClick={this.handleClick} className="button is-large">General</button>
-            <button onClick={this.handleClick} className="button is-large">Business</button>
-            <button onClick={this.handleClick} className="button is-large">Technology</button>
-            <button onClick={this.handleClick} className="button is-large">Entertainment</button>
-            <button onClick={this.handleClick} className="button is-large">Health</button>
-            <button onClick={this.handleClick} className="button is-large">Sport</button> */}
+        <section id={!this.state.loading && 'hero-loading'}>
+          <div className="header"></div>
+          <div className="navbar topstory-nav form-container" role="navigation" aria-label="sub navigation">
+            <div className="navbar-brand">
+              <SubNavBtns onClick={this.handleClick} className="button is-large"/>
+            </div>
           </div>
-        </div>
-        <div className='outer-top-stories'>
-          <div className="news-grid">
-            {this.state.stories && this.state.stories.articles.map((article, i) => <NewsCard key={i} {...article} />)}
-          </div>
-        </div> 
+          <div className='outer-top-stories'>
+            <div className="loading" id={this.state.loading && 'is-loading'}>LOADING</div>
+            <div className="news-grid">
+              {this.state.stories && this.state.stories.articles.map((article, i) => <NewsCard key={i} {...article} />)}
+            </div>
+          </div> 
+        </section>
       </>
     )
   }
