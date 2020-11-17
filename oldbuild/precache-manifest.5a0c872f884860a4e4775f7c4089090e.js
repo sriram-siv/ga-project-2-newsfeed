4b85da590b98d81bf7d5eb11107ed63e76fd12a7
Sriram Sivarajah <sriramsivarajah@gmail.com>
2020-11-17 18:43:39 +0000

    adjust settings for prod

---
+++ oldbuild/precache-manifest.5a0c872f884860a4e4775f7c4089090e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01a21e2980e98509f6a12befa9314282",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecda2d9b8083359354b4",
+    "url": "/static/css/main.c9e6b2de.chunk.css"
+  },
+  {
+    "revision": "af60d9d8b1a3c0531fed",
+    "url": "/static/js/2.c8806542.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c8806542.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecda2d9b8083359354b4",
+    "url": "/static/js/main.25cb1c06.chunk.js"
+  },
+  {
+    "revision": "bde3d8b298d96b843953",
+    "url": "/static/js/runtime-main.7e3e9db2.js"
+  }
+]);
